b0cd91e7e0f27c951c83c63d072b07e1435a3cd2
gui <guihori2000@hotmail.com>
2018-11-15 12:53:31 -0500

    Add files via upload

--- createParticle.m
+++ createParticle.m
@@ -1,5 +1,9 @@
 function particle = createParticle(speed,angle,xPos,yPos)
+
 particle.speed = speed;
+
 particle.angle = angle;
+
 particle.xPos = xPos;
+
 particle.yPos = yPos;
--- run.m
+++ run.m
@@ -1,10 +1,37 @@
-while true
-    clf
-    for i = 1:length(a)
-        a(i) = particleUpdate(a(i),1);
+r = 5; %radius of circle
+figure('Position', [100,100,600,500])
+
+
+for q = 1:1000 
+    
+    
+    for i = 1:length(particleList)
+        if particleList(i).xPos >= 100 - r
+            particleList(i).angle = 180 - particleList(i).angle;
+        elseif particleList(i).xPos <= r
+            particleList(i).angle = 180 - particleList(i).angle;
+        end
+        if particleList(i).yPos >= 100 - r
+            particleList(i).angle = -1 * particleList(i).angle;
+        elseif particleList(i).yPos <= r
+            particleList(i).angle = -1 * particleList(i).angle;
+        end
+        
+        %update position of particles
+        particleList(i) = particleUpdate(particleList(i),1);
     end
-    for i = 1:length(a)
-        circle(a(i).xPos,a(i).yPos,5)
+        
+    
+    %clear canvas
+    %clf
+    
+    %temporary solution to fix the window size
+    plot([-5,105],[-5,105])
+    
+    %draw particle
+    for i = 1:length(particleList)
+        circle(particleList(i).xPos,particleList(i).yPos,r)
     end
+    
     drawnow
 end
