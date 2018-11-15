3ab95a4395349baeca877db10ad8d234f7c0cf37
21oglethorpe <45078602+21oglethorpe@users.noreply.github.com>
2018-11-15 14:20:35 -0500

    Update collisionTest.m
    added comments

--- collisionTest.m
+++ collisionTest.m
@@ -1,9 +1,7 @@
 function collisionTest(partA,partB)
+%measures distance from particleA and particleB
 distance = sqrt((partA.xPos-partB.xPos)^2+(partA.yPos-partB.yPos)^2);
-
-
-
-
+%particles collide if distance is <= sum of the 2 particles' radius
 if(distance <= partA.radius+partB.radius)
     collision(partA,partB);
 end
