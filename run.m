cf9c7094ca85269a62be9ba3c2f6fad7c1bbc8a4
guihori <guihori2000@hotmail.com>
2018-11-26 14:35:18 -0500

    Enabled collision

--- run.m
+++ run.m
@@ -3,8 +3,7 @@ figure('Position', [100,100,600,500])
 
 
 while true
-    
-    
+
     for i = 1:length(particleList)
         if particleList(i).xPos >= 100 - particleList(i).radius
             particleList(i).angle = 180 - particleList(i).angle;
@@ -16,30 +15,30 @@ while true
         elseif particleList(i).yPos <= particleList(i).radius
             particleList(i).angle = -1 * particleList(i).angle;
         end
-        
+
         %update position of particles
         particleList(i) = particleUpdate(particleList(i),1);
     end
-        
-    
+
+
     %clear canvas
     %clf
-    
+
     %temporary solution to fix the window size
     plot([-5,105],[-5,105]);
-    
+
     %draw particle
     for i = 1:length(particleList)
         circle(particleList(i).xPos,particleList(i).yPos,particleList(i).radius);
     end
-    
+
     %Test for collision of all combinations of particles 
 
    if(length(particleList)>1)
         for i = 1:length(particleList)-1
             for j =i+1:length(particleList)
-                collisionTest(particleList(i),particleList(j));
-                
+                [particleList(i), particleList(j)] = collisionTest(particleList(i),particleList(j));
+
                 %finds time to collision, displays time if its less than 1
                 timeToCollision = (-sqrt((2 * particleList(i).xPos * particleList(i).speed * cosd(particleList(i).angle)...
                     - 2 * particleList(i).xPos * particleList(j).speed * cosd(particleList(j).angle) - 2 * particleList(j).xPos...
@@ -66,12 +65,12 @@ while true
                     * cosd(particleList(i).angle)^2 + particleList(j).speed^2 * sind(particleList(j).angle)^2 + particleList(j).speed^2 ...
                     * cosd(particleList(j).angle)^2));
                 if timeToCollision >= 0 && timeToCollision < 1 && isreal(timeToCollision)
-                    disp(timeToCollision)
+
                 end
 
             end
         end
-    end
+   end
     
     drawnow
 end
